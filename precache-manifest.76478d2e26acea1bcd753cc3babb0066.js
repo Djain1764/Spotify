7d9a7da9a1e5e58155302931615cf35e7e4719cb
Djain1764 <djain1764@gmail.com>
2022-11-02 17:54:22 +0530

    Updates

---
+++ precache-manifest.76478d2e26acea1bcd753cc3babb0066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d22c8530776382a2004517ccd1561bd",
+    "url": "/Spotify/index.html"
+  },
+  {
+    "revision": "48cda2463853d03aaca8",
+    "url": "/Spotify/static/css/main.c8e2e44e.chunk.css"
+  },
+  {
+    "revision": "f98af3001e271d56d837",
+    "url": "/Spotify/static/js/2.bb18f34d.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Spotify/static/js/2.bb18f34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cda2463853d03aaca8",
+    "url": "/Spotify/static/js/main.986cbba1.chunk.js"
+  },
+  {
+    "revision": "2ca866082d361f3d6767",
+    "url": "/Spotify/static/js/runtime-main.a6619ebf.js"
+  }
+]);
